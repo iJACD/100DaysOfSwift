9b097cf531fece03dc7f197c3bfedd5a142b3042
JohnAnthony Dobson <jcdobson2@gmail.com>
2019-04-01 23:55:02 -0400

    Day 1: Strings, simple data types and String interpolation.

---
+++ Day 1.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Cocoa
+
+var str0 = "Hello, playground"
+str0 = "Good Bye"
+
+//swift allows for underscores as thousands separators
+var population = 1_000_000
+
+//back slash with tripple quotes escape new line
+var str1 = """
+This will
+break the lines
+up into three.
+"""
+
+var str2 = """
+Using the backslash \
+does not allow for the \
+line break.
+"""
+
